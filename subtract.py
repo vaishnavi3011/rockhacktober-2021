4c1ea7240baaa000fb264bdc063332344a2d1540
Abid Hussain <abid.hussain@contour-software.com>
2021-10-04 14:17:26 +0500

    feat: subtract program added

---
+++ subtract.py
@@ -0,0 +1,10 @@
+# This program subtracts two numbers
+
+num1 = 10.7
+num2 = 5.1
+
+# Subtract two numbers
+diff = num1 - num2
+
+# Display the diff
+print('The diff of {0} and {1} is {2}'.format(num1, num2, diff))
